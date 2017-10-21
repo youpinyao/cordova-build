484114548046db1fffd722d35f8b0356298804dd
游品尧 <497400448@qq.com>
2017-10-21 21:28:55 +0800

    1

--- getCIP.js
+++ getCIP.js
@@ -1,30 +1,4 @@
-var os = require('os');
 
+var ip = require('ip');
 
-var CIP = null;
-
-var network = os.networkInterfaces();
-
-if (os.platform() == 'win32') {
-	for (var n in network) {
-		var item = network[n];
-
-		for (var i = 0; i < item.length; i++) {
-			var json = item[i];
-			if (json.family == 'IPv4' && json.address != '127.0.0.1') {
-				CIP = json.address;
-			}
-		}
-	}
-} else {
-  var en = network.en0 || network.en1;
-	for (var i = 0; i < en.length; i++) {
-		var json = en[i];
-		if (json.family == 'IPv4' && json.address != '127.0.0.1') {
-			CIP = json.address;
-		}
-	}
-}
-
-
-module.exports = CIP;
+module.exports = ip.address();
