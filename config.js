5d44d3e916229dc0ed397156b0810786855f5a0a
游品尧 <497400448@qq.com>
2018-02-22 10:10:12 +0800

    ip 修改

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var CIP = require('./getCIP.js');
 var UPORT = '12345';
 var SPORT = '54321';
 
-var SIP = '192.168.3.11'
+var SIP = '192.168.3.254'
 // var SIP = '10.1.199.128'
 
 module.exports = {
